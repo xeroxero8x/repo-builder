a90f5bb9e44c6d5438a114fa9139d1c407a332dd
008 <xeroxero8@outlook.com>
2024-08-11 23:43:15 +0600

    fixed: typo in the url

--- scripts/variables.sh
+++ scripts/variables.sh
@@ -3,7 +3,7 @@
 #Difines all the variables here
 
 GIT_USERNAME=xeroxero8x
-REPO_DIR=aur-prebuit
+REPO_DIR=aur-prebuilt
 REPO_URL=gitlab.com/$GIT_USERNAME/$REPO_DIR
 AUR_DIR=aur-packages
 ARCH=x86_64
