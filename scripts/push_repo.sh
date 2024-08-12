4caa58f6f3a09720d7a1e2ba41cf68bb47eb280c
008 <xeroxero8@outlook.com>
2024-08-12 22:09:16 +0600

    updated: PAT approach

--- scripts/push_repo.sh
+++ scripts/push_repo.sh
@@ -6,8 +6,8 @@ source scripts/variables.sh
 git config --global user.name "$GIT_USERNAME"
 git config --global user.email "$GIT_EMAIL"
 cd $REPO_DIR
-ls -la 
+ls -la
 echo "Pushing changes to GitLab"
 git add .
 git commit -m "auto: Update packages and database"
-git push https://$GIT_USERNAME:${{secrets.PAT}}@$REPO_URL
+git push https://$GIT_USERNAME:$PAT@$REPO_URL
