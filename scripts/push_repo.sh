ff38f67a232005cc82f44818e83338e7902febfe
008 <xeroxero8@outlook.com>
2024-08-12 21:08:19 +0600

    log: check

--- scripts/push_repo.sh
+++ scripts/push_repo.sh
@@ -5,8 +5,9 @@ set -e # Exit immediately if a command exits with a non-zero status
 # Configure git
  git config --global user.name "$GIT_USERNAME"
 git config --global user.email "$GIT_EMAIL"
-
+ls -la 
 cd $REPO_DIR
+ls -la 
 echo "Pushing changes to GitLab"
  git add .
 git commit -m "auto: Update packages and database"
