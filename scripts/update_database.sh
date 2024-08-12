314560200428beaea2531ef41e4d094bf3a87031
008 <xeroxero8@outlook.com>
2024-08-12 11:23:20 +0600

    fixed: path to sh scripts

--- scripts/update_database.sh
+++ scripts/update_database.sh
@@ -3,7 +3,7 @@
 set -e # Exit immediately if a command exits with a non-zero status
 
 #sourcing Variables
-source ./variables.sh
+source scripts/variables.sh
 
 # Check for the presence of the database files
 cd $REPO_DIR
