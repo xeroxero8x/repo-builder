e192210f2b91d0187a2c5d8a8e046d96946ea738
008 <xeroxero8@outlook.com>
2024-08-24 16:18:40 +0600

    update

--- scripts/build_packages.sh
+++ scripts/build_packages.sh
@@ -2,14 +2,11 @@
 
 set -e # Exit immediately if a command exits with a non-zero status
 
-
 # Check if package list file exists
 if [[ ! -f $PACKAGE_LIST ]]; then
   echo "Package list file $PACKAGE_LIST not found!"
   exit 1
 fi
-
-
 
 # Read package names from the list and build each package
 while IFS= read -r pkg; do
@@ -21,12 +18,11 @@ while IFS= read -r pkg; do
   fi
 
   echo "Building package: $pkg"
- 
-#Building the package with aurutils
-aur sync --noview --noconfirm $pkg || {
-  echo "Failed to Build $pkg
-  continue
-}
+
+  #Building the package with aurutils
+  aur sync --noview --noconfirm $pkg || {
+    echo "Failed to Build $pkg "
+    continue
+  }
 
 done <"../$PACKAGE_LIST"
-
