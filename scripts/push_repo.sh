73e9c258d4627add8d6a6285ab5fe8079f4cd1da
008 <xeroxero8@outlook.com>
2024-08-24 16:57:26 +0600

    forget about the path variable in linux

--- scripts/push_repo.sh
+++ scripts/push_repo.sh
@@ -5,7 +5,7 @@ set -e # Exit immediately if a command exits with a non-zero status
 # Configure git
 git config --global user.name "$GIT_USERNAME"
 git config --global user.email "$GIT_EMAIL"
-cd $PATH/$REPO_DIR
+cd $XXXX/$REPO_DIR
 echo "Pushing changes to GitLab"
 git add .
 git commit -m "auto: Update packages and database"
