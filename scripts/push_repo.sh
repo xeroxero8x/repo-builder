47894d3d16b8d47176b761d5fcb127512970be6c
008 <xeroxero8@outlook.com>
2024-09-01 22:19:00 +0600

    test

--- scripts/push_repo.sh
+++ scripts/push_repo.sh
@@ -2,6 +2,8 @@
 
 set -e # Exit immediately if a command exits with a non-zero status
 
+cd $USER_HOME/$REPO_DIR
+ls -la
 # Configure git
 git_config() {
     git config --global user.name "$GIT_USERNAME"
@@ -19,7 +21,7 @@ check_git_lfs_installed() {
 
 # Function to find .zst files larger than 100MB and track them with Git LFS
 track_large_files() {
-    for file in $(find "$REPO_DIR/$ARCH" -name "*.zst"); do
+    for file in $(find "$ARCH" -name "*.zst"); do
         filesize=$(du -b "$file" | cut -f1)
         if (( filesize > 104857600 )); then
             echo "File $file is larger than 100MB ($((filesize / 1048576))MB)."
@@ -35,9 +37,6 @@ track_large_files() {
 
 # Function to commit and push changes
 commit_and_push() {
-    echo "Moving to the $REPO_DIR"
-    cd $USER_HOME/$REPO_DIR
-    ls -la  
     echo "Committing changes..."
     git commit -m "Add files, tracking large files with Git LFS"
     echo "Pushing to the remote repository..."
@@ -48,7 +47,6 @@ commit_and_push() {
 # Main script
 main() {
     git_config
-    cd $USER_HOME
     check_git_lfs_installed
     track_large_files
     commit_and_push
