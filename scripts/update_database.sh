cfd4312f6564cc9d7e6fddae83b0ff477fb926c0
008 <xeroxero8@outlook.com>
2024-09-02 13:43:14 +0600

    script: updated database updater

--- scripts/update_database.sh
+++ scripts/update_database.sh
@@ -2,12 +2,14 @@
 
 set -e # Exit immediately if a command exits with a non-zero status
 
-# Check for the presence of the database files
-cd $USER_HOME/$REPO_DIR
-ls -la $ARCH
+# Change to the repository directory
+cd "$USER_HOME/$REPO_DIR"
+
+# Check for the presence of the database files and rename them if found
 if [[ -f "$ARCH/$DB_FILE" && -f "$ARCH/$FILES_FILE" ]]; then
-  echo "Database files found. Deleting them..."
-  rm "$ARCH/$DB_FILE" "$ARCH/$FILES_FILE"
+  echo "Database files found. Renaming them with a .old extension..."
+  mv "$ARCH/$DB_FILE" "$ARCH/$DB_FILE.old"
+  mv "$ARCH/$FILES_FILE" "$ARCH/$FILES_FILE.old"
 else
   echo "Database files not found."
 fi
@@ -16,9 +18,9 @@ fi
 echo "Running repo-add to update the repository..."
 repo-add "$ARCH/$DB_FILE.tar.gz" "$ARCH"/*.pkg.tar.zst
 
-# Rename the newly created database files
-echo "Renaming the new database files..."
-mv "$ARCH/$DB_FILE.tar.gz" "$ARCH/$DB_FILE"
-mv "$ARCH/$FILES_FILE.tar.gz" "$ARCH/$FILES_FILE"
+# Copy the newly created database and files files
+echo "Copying the new database and files files..."
+cp "$ARCH/$DB_FILE.tar.gz" "$ARCH/$DB_FILE"
+cp "$ARCH/$FILES_FILE.tar.gz" "$ARCH/$FILES_FILE"
 
 echo "Repository update complete."
