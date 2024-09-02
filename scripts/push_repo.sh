510f199228694db27bb1d10b8cabbae9c66b367b
008 <xeroxero8@outlook.com>
2024-09-02 21:56:02 +0600

    scripts: fixed git lfs error

--- scripts/push_repo.sh
+++ scripts/push_repo.sh
@@ -2,14 +2,14 @@
 
 set -e # Exit immediately if a command exits with a non-zero status
 
-#Movig to repo directory
-cd $USER_HOME/$REPO_DIR
+# Moving to repo directory
+cd "$USER_HOME/$REPO_DIR"
 
 # Configure git
 git_config() {
     git config --global user.name "$GIT_USERNAME"
     git config --global user.email "$GIT_EMAIL"
-    git config --global --add safe.directory $USER_HOME/$REPO_DIR
+    git config --global --add safe.directory "$USER_HOME/$REPO_DIR"
 }
 
 # Function to check if Git LFS is installed
@@ -18,8 +18,8 @@ check_git_lfs_installed() {
         echo "Git LFS is not installed. Attempting to install..."
         sudo pacman -S git-lfs --noconfirm
         git lfs install
-        else
-            git lfs install
+    else
+        git lfs install
     fi
 }
 
@@ -31,6 +31,7 @@ track_large_files() {
             echo "File $file is larger than 100MB ($((filesize / 1048576))MB)."
             echo "Tracking $file with Git LFS..."
             git lfs track "$file"
+            git add "$file"
             git add .gitattributes
         else
             echo "File $file is under 100MB ($((filesize / 1048576))MB). Adding normally..."
@@ -44,8 +45,8 @@ commit_and_push() {
     echo "Committing changes..."
     git commit -m "Add files, tracking large files with Git LFS"
     echo "Pushing to the remote repository..."
-    git push https://$GIT_USERNAME:$PAT@$REPO_URL
-
+    git push https://"$GIT_USERNAME":"$PAT"@"$REPO_URL"
+    git lfs push --all https://"$GIT_USERNAME":"$PAT"@"$REPO_URL"
 }
 
 # Main script
