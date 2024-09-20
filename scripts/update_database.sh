3797d06441a2afa98599f9d97b77fa1e99e7b83d
008 <xeroxero8@outlook.com>
2024-09-20 15:13:36 +0600

    script: updated db updater

--- scripts/update_database.sh
+++ scripts/update_database.sh
@@ -7,9 +7,9 @@ cd "$USER_HOME/$REPO_DIR"
 
 # Check for the presence of the database files and rename them if found
 if [[ -f "$ARCH/$DB_FILE" && -f "$ARCH/$FILES_FILE" ]]; then
-  echo "Database files found. Renaming them with a .old extension..."
-  mv "$ARCH/$DB_FILE" "$ARCH/$DB_FILE.old"
-  mv "$ARCH/$FILES_FILE" "$ARCH/$FILES_FILE.old"
+  echo "Database files found. Removing them
+  rm "$ARCH/$DB_FILE.tar.gz"
+  rm "$ARCH/$FILES_FILE.tar.gz"
 else
   echo "Database files not found."
 fi
